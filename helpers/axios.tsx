abffabd3a933c8a0adb38bbc257da49156e6617f
fran <francisco.vento@outlook.com>
2022-09-28 02:12:38 -0300

    feat: adding axios to proyect

---
+++ helpers/axios.tsx
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import getConfig from 'next/config';
+
+const { publicRuntimeConfig } = getConfig();
+const BASE_URL = publicRuntimeConfig.BASE_URL;
+const TOKEN = typeof window !== 'undefined' ? '' : '';
+
+const instance = axios.create({
+  baseURL: BASE_URL,
+  headers: { Authorization: `Bearer ${TOKEN}` },
+});
+
+instance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error?.response?.status === 401) {
+      console.log(error);
+      Promise.reject(error);
+      return;
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,4 @@
-import Image from 'next/image';
-import Link from 'next/link';
 import { useRouter } from 'next/router';
-import CatCard from '../components/cards/cat/CatCard';
-import { mockCatCardProps } from '../components/cards/cat/CatCard.mocks';
 import PrimaryLayout from '../components/layouts/primary/PrimaryLayout';
 // import Search from '../components/utility/search/Search';
 import { NextPageWithLayout } from './page';
@@ -10,26 +6,7 @@ import { NextPageWithLayout } from './page';
 const Home: NextPageWithLayout = () => {
   const { locale } = useRouter();
 
-  return (
-    <section className="flex flex-col items-center gap-y-5 mt-12 sm:mt-36">
-      <div className="p-2 ">locasdl asdlkna jskdn akljsf na</div>
-      <Image
-        src="/time-cat.jpg"
-        alt="Google Logo"
-        width={272}
-        height={92}
-        priority
-      />
-      {/* <Search /> */}
-      <CatCard {...mockCatCardProps.base} />
-      <p>
-        Google offered in:{' '}
-        <Link href="/" locale={locale === 'en' ? 'fr' : 'en'}>
-          <a className="underline text-blue-600"> Français</a>
-        </Link>
-      </p>
-    </section>
-  );
+  return <div></div>;
 };
 
 export default Home;
