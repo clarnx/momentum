5bb02a10e2465fd765b79b92c699b1abcbbde6ef
fran <francisco.vento@outlook.com>
2022-09-29 20:14:22 -0300

    fix: home props

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,24 @@
 import { useAuth0 } from '@auth0/auth0-react';
-import { useRouter } from 'next/router';
 import { NextPageWithLayout } from './page';
 
+import { GetStaticProps } from 'next';
 import LoginButton from '../components/buttons/Login';
 import LogoutButton from '../components/buttons/logout';
 import PrimaryLayout from '../components/layouts/primary/PrimaryLayout';
 import en from '../locales/en/home';
 import es from '../locales/es/home';
 
-const Home: NextPageWithLayout = () => {
+interface IHome {
+  locale: string;
+}
+
+const Home: NextPageWithLayout<IHome> = ({ locale }) => {
   const { user, isAuthenticated, isLoading } = useAuth0();
 
   console.log({ user, isAuthenticated, isLoading });
 
-  const router = useRouter();
-  const { locale } = router;
+  // const router = useRouter();
+  // const { locale } = router;
 
   const t = locale === 'en' ? en : es;
   return (
@@ -38,4 +42,12 @@ export default Home;
 
 Home.getLayout = (page) => {
   return <PrimaryLayout pageTitle="Inicio">{page}</PrimaryLayout>;
+};
+
+export const getStaticProps: GetStaticProps = ({ locale }) => {
+  return {
+    props: {
+      locale,
+    },
+  };
 };
